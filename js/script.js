244d311a392b002b5da48273fac7b288ba710b5b
Myxogastria0808 <r.rstudio.c@gmail.com>
2023-01-01 15:05:52 +0900

    Hello

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ window.onload = ()=>{
     const loader = document.getElementById('loader');
     loader.classList.add('loaded');
 }
+
+var _window = $(window),
+    _header = $('.site-header'),
+    heroBottom;
+ 
+_window.on('scroll',function(){
+    heroBottom = $('.title-h1').height();
+    if(_window.scrollTop() > heroBottom){
+        _header.addClass('transform');   
+    }
+    else{
+        _header.removeClass('transform');   
+    }
+});
+ 
+_window.trigger('scroll');
